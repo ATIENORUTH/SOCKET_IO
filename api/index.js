b9348984e21cb83d9c4434ae1a3a93ce91a44248
DEVLYN <devlynruth@gmail.com>
2025-07-12 15:32:29 +0300

    Fix Vercel runtime configuration error

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 // Vercel API route for Socket.IO chat application
 const express = require('express');
-const { Server } = require('socket.io');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
